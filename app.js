79c81a9a790e61fc9c5f7bb0fecc61d027eef8a9
Pratik Suryawanshi <spdpratik17@gmail.com>
2017-10-09 19:02:38 -0700

    landing get req

--- app.js
+++ app.js
@@ -4,29 +4,15 @@ var app             = express();
 
 app.set("view engine","ejs");
 
-app.get('/',function(req,res){
-    res.render("landing");
+var BooksJSON = '{"books":[{"id":"book1","name":"The Image-Guided Surgical Toolkit","price":"0.99","url":"http://www.igstk.org/IGSTK/help/documentation.html"},{"id":"book2","name":"Abraham Lincoln","price":"19.95","url":"http://www.learnlibrary.com/abraham-lincoln/lincoln.htm"},{"id":"book3","name":"Adventures of Tom Sawyer","price":"10.50","url":"http://www.pagebypagebooks.com/Mark_Twain/Tom_Sawyer/"},{"id":"book4","name":"Catcher in the Rye","price":"22.95","url":"https://www.goodreads.com/book/show/5107.The_Catcher_in_the_Rye"},{"id":"book5","name":"The Legend of Sleepy Hollow","price":"15.99","url":"http://www.learnlibrary.com/sleepy-hollow/sleepy-hollow.htm"},{"id":"book6","name":"Moby Dick","price":"24.45","url":"https://www.amazon.com/Moby-Dick-Herman-Melville/dp/1503280780"},{"id":"book7","name":"Java Programming 101","price":"1.99","url":"https://www.javaworld.com/blog/java-101/"},{"id":"book8","name":"Robinson Crusoe","price":"11.99","url":"http://www.learnlibrary.com/rob-crusoe/"},{"id":"book9","name":"The Odyssey","price":"32.00","url":"http://classics.mit.edu/Homer/odyssey.html"}]}';
+
+app.get('/landing',function(req,res){
+  res.render("landing");
 });
 
 //===============================================================
+
 app.listen(8080, process.env.IP, function(){
     console.log("Server started...");
 });
 
-app.get('/', function (req, res) {
-    var response = '<html><head><title>Simple Send</title></head>' +
-                   '<body><h1>Hello from Express</h1></body></html>';
-    res.status(200);
-    res.set({
-      'Content-Type': 'text/html',
-      'Content-Length': response.length
-    });
-    res.send(response);
-    console.log('Response Finished? ' + res.finished);
-    console.log('\nHeaders Sent: ');
-    console.log(res.headersSent);
-  });
-  app.get('/error', function (req, res) {
-    res.status(400);
-    res.send("This is a bad request.");
-  });
