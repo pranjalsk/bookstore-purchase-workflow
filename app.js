4a00880cc343a78d237db88adaee90d7a2aa55d4
Pratik Suryawanshi <spdpratik17@gmail.com>
2017-10-09 18:57:15 -0700

    book json

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ var bodyParser      = require("body-parser");
 var app             = express();
 
 app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function(req,res){
     res.render("landing");
 });
 
 //===============================================================
-app.listen(8000, function(){
+app.listen(8080, process.env.IP, function(){
     console.log("Server started...");
 });
+
+app.get('/', function (req, res) {
+    var response = '<html><head><title>Simple Send</title></head>' +
+                   '<body><h1>Hello from Express</h1></body></html>';
+    res.status(200);
+    res.set({
+      'Content-Type': 'text/html',
+      'Content-Length': response.length
+    });
+    res.send(response);
+    console.log('Response Finished? ' + res.finished);
+    console.log('\nHeaders Sent: ');
+    console.log(res.headersSent);
+  });
+  app.get('/error', function (req, res) {
+    res.status(400);
+    res.send("This is a bad request.");
+  });
