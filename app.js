2f1cde61ef7881bf913d02f46f8a149226652565
pranjalsk <pkaranka@asu.edu>
2017-10-09 17:07:13 -0700

    initial setup

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express         = require("express");
 var bodyParser      = require("body-parser");
 var app             = express();
 
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',function(req,res){
+    res.send("Hi from server");
+});
 
 //===============================================================
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(8000, function(){
     console.log("Server started...");
 });
