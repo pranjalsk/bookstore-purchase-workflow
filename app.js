aa4b5d4fc5db2ed9f4f8b10ab916b9f0aebce3a3
pranjalsk <pkaranka@asu.edu>
2017-10-09 17:10:56 -0700

    push 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function(req,res){
-    res.send("Hi from server");
+    res.render("landing");
 });
 
 //===============================================================
